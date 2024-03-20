d2110ef88c57b7641025646023b4fef475f9740c
artem_bakhmatov <ip.bakhmatov@mail.ru>
2024-03-20 21:10:46 +0300

    Project Setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Hero from "./components/Hero"
+import HighLights from "./components/HighLights"
+import Navbar from "./components/Navbar"
+
+const App = () => {
+		
+	return (
+		<main className="bg-black">
+			<Navbar />
+			<Hero />
+			<HighLights />
+		</main>
+	)
+		
+}
+
+export default App
---
+++ src/components/HighLights.jsx
@@ -0,0 +1,9 @@
+const HighLights = () => {
+    return (
+        <div>
+            HighLights
+        </div>
+    );
+};
+
+export default HighLights;
